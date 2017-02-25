e9cd5b9b09af9b9f946390116c540f65c95acde0
tylerbadams <tylerbadams@gmail.com>
2017-02-25 16:52:31 -0700

    ...

--- install.sh
+++ install.sh
@@ -8,5 +8,6 @@ git checkout -f greedyMarket-aggregate/master
 
 if ! grep -Fxq "0 0 */1 * * ${PWD}/deploy.py" /var/spool/cront/root; then
    echo "0 0 */1 * * ${PWD}/deploy.py" | tee -a /var/spool/cron/root
+fi
 
 python deploy.py
