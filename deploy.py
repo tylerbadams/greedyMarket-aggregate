19b4ea37aa082cbe361918c382157e530c47f1d9
tylerbadams <tylerbadams@gmail.com>
2017-02-25 16:07:48 -0700

    ...

--- deploy.py
+++ deploy.py
@@ -10,7 +10,7 @@ print deployConf
 repo = git.Repo(os.getcwd())
 repo.remotes['greedyMarket-aggregate'].fetch( verbose = True )
 
-commits_behind = repo.iter_commits('HEAD..origin/{0}'.format(deployConf['branch']))
+commits_behind = repo.iter_commits('HEAD..greedyMarket-aggregate/{0}'.format(deployConf['branch']))
 
 if len([1 for c in commits_behind]) > 0:
   repo.git.checkout('remotes/origin/{0}'.format(deployConf['branch']))
